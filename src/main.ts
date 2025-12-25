49ad587a9f4f96d439e6e8e056de0713da17c2f8
Kharyl Ortiz <kharylortiz@gmail.com>
2025-12-25 23:42:03 +0800

    update styles

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
-createApp(App).mount('#app')
+import Button from 'primevue/button'
+
+//
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.component('Button', Button)
+app.mount('#app')
